a7f4553d69d9fd2d7a0fc0c72f8260720c81e171
Raul Akhmetshin <rakhmetshin@nvidia.com>
2026-09-10 16:29:26 +0300

    UCT/CUDA/CUDA_COPY: Updated doc.

--- src/uct/cuda/cuda_copy/cuda_copy_md.h
+++ src/uct/cuda/cuda_copy/cuda_copy_md.h
@@ -113,7 +113,9 @@ uct_cuda_copy_md_dmabuf_t uct_cuda_copy_md_get_dmabuf(const void *address,
  * @param [in]     enable_fabric Controls fabric VMM allocation
  * @param [in]     cu_device     CUDA device to allocate memory on
  * @param [in]     length        The minimal size to allocate
- * @param [in,out] granularity_p Allocation granularity, if fabric VMM is used
+ * @param [in,out] granularity_p Fabric VMM allocation granularity. If the
+ *                               pointed-to value is SIZE_MAX, the granularity
+ *                               is queried and written back.
  * @param [out]    alloc_handle  Filled with information about the allocated
  *                               memory
  */
